c82a895b9e8f405377e879ca1b88fb2921606c90
Bartok <thom.huenger@gmail.com>
2021-07-05 08:52:41 -0400

    again

--- myApp.js
+++ myApp.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-const absolutePath = __dirname + "/views/index.html"
+
 
 console.log("Hello World")
 
 
 
 app.get("/", (req, res) => {
-    res.sendFile(absolutePath);
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
