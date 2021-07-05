bbf43336d6dbb69b0e046c844a8e7520f5adef29
Bartok <thom.huenger@gmail.com>
2021-07-05 08:50:33 -0400

    second try

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const absolutePath = __dirname + "views/index.html"
+const absolutePath = __dirname + "/views/index.html"
 
 console.log("Hello World")
 
